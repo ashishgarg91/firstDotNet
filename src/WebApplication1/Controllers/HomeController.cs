8435e632f96764a8f65d4974701e92b3a14670bb
ashishgarg91 <agarg.ec13@gmail.com>
2018-07-14 16:28:18 +0530

    commit5

--- src/WebApplication1/Controllers/HomeController.cs
+++ src/WebApplication1/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace WebApplication1.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your contact page.Hi Ashish";
 
             return View();
         }
