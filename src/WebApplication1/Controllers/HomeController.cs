f8419fd1eb7af01e13a91fba9f9ba83e6a77a37b
ashishgarg91 <agarg.ec13@gmail.com>
2018-07-14 15:32:51 +0530

    commit2

--- src/WebApplication1/Controllers/HomeController.cs
+++ src/WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.Hi Ashish";
+            ViewData["Message"] = "Your application description .";
 
             return View();
         }
