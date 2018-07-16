db118ffec8033a6faaeaa73212ff7df68dfcf87e
ashishgarg91 <agarg.ec13@gmail.com>
2018-07-16 12:41:13 +0530

    Update HomeController.cs

--- src/WebApplication1/Controllers/HomeController.cs
+++ src/WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description.test";
+            ViewData["Message"] = "Your application description.test1";
 
             return View();
         }
